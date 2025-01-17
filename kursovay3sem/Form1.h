1d2b0d5060292661be8a52f12506d9030c0e34c6
Danila <silnovdanila2@gmail.com>
2025-01-17 09:02:17 +0300

    Добавьте файлы проекта.

---
+++ kursovay3sem/Form1.h
@@ -0,0 +1,204 @@
+#pragma once
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+		}
+
+	protected:
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button2;
+
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Label^ label4;
+
+	private:
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 28));
+			this->button1->Location = System::Drawing::Point(325, 136);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(344, 175);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Сделать заказ";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 30));
+			this->label1->Location = System::Drawing::Point(410, 32);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(160, 58);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Меню";
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8));
+			this->button2->Location = System::Drawing::Point(708, 326);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(117, 50);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Поменять режим";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->label2->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->label2->Location = System::Drawing::Point(752, 32);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(188, 20);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Режим пользователя";
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8));
+			this->button3->Location = System::Drawing::Point(844, 326);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(117, 50);
+			this->button3->TabIndex = 5;
+			this->button3->Text = L"Режим рахработчика";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Visible = false;
+			this->button3->Click += gcnew System::EventHandler(this, &Form1::button3_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(705, 396);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(113, 16);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Введите пароль";
+			this->label3->Visible = false;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(708, 415);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(110, 22);
+			this->textBox1->TabIndex = 7;
+			this->textBox1->Visible = false;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(708, 443);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(110, 23);
+			this->button4->TabIndex = 8;
+			this->button4->Text = L"Подтвердить";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Visible = false;
+			this->button4->Click += gcnew System::EventHandler(this, &Form1::button4_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(701, 478);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(124, 16);
+			this->label4->TabIndex = 9;
+			this->label4->Text = L"Пароль неверный";
+			this->label4->Visible = false;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1014, 536);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Name = L"Form1";
+			this->Text = L"Cafe";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Visible = true;
+		this->label3->Visible = true;
+		this->button4->Visible = true;
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	if (this->textBox1->Text == L"1111") {
+		this->textBox1->Visible = false;
+		this->label3->Visible = false;
+		this->button4->Visible = false;
+		this->button2->Visible = false;
+		this->button3->Visible = true;
+		this->label2->Text = L"Режим разработчика";
+		this->label2->ForeColor = System::Drawing::SystemColors::MenuHighlight;
+	}
+	else {
+		this->label4->Visible = true;
+	}
+}
+};
+}
